1becded704bbe9bc7eb0961f505331e603866b06
Jonathan Mindel <jm2889@princeton.edu>
2023-10-16 21:27:43 -0400

    deployed database and created .env file

---
+++ profdb.py
@@ -0,0 +1,10 @@
+import os
+# import psycopg2
+import dotenv
+
+#-----------------------------------------------------------------------
+
+dotenv.load_dotenv()
+_DATABASE_URL = os.environ['DATABASE_URL']
+
+#-----------------------------------------------------------------------
