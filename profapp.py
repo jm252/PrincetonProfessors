3bbc092eb8448c1d1b82d0a33b18d76809257530
Eden Bendory <eb1889@princeton.edu>
2023-10-26 17:29:02 -0400

    adds cas app routes

--- profapp.py
+++ profapp.py
@@ -1,9 +1,22 @@
-import html # html_code.escape() is used to thwart XSS attacks
+# import html # html_code.escape() is used to thwart XSS attacks
 import flask
+import auth
 
 #-----------------------------------------------------------------------
 
 app = flask.Flask(__name__, template_folder='.')
+
+#-----------------------------------------------------------------------
+
+# Routes for authentication.
+
+@app.route('/logoutapp', methods=['GET'])
+def logoutapp():
+    return auth.logoutapp()
+
+@app.route('/logoutcas', methods=['GET'])
+def logoutcas():
+    return auth.logoutcas()
 
 #-----------------------------------------------------------------------
 
