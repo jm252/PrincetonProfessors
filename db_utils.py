2125e5e157bae76afa20980499ba65c9cdda3063
Kayla Waitman <kw2689@princeton.edu>
2023-11-09 21:31:58 -0500

    edits to db_utils to add fields to each professor object

--- db_utils.py
+++ db_utils.py
@@ -20,39 +20,55 @@ except Exception as ex:
 
 
 def get_all_professors():
-    with sqlalchemy.orm.Session(engine) as session:
-        query = session.query(Professor)
-        table = query.all()
-        return table
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            query = session.query(Professor)
+            table = query.all()
+            return table
+    except sqlalchemy.exc.SQLAlchemyError as ex:
+        print(f"Error retrieving all professors: {ex}", file=sys.stderr)
+
 
 
 def get_professor(name: str):
-    with sqlalchemy.orm.Session(engine) as session:
-        query = session.query(Professor).filter(name == name)
-        professor = query.all()
-        return professor
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            query = session.query(Professor).filter(Professor.name == name)
+            professor = query.all()
+            return professor
+    except sqlalchemy.exc.SQLAlchemyError as ex:
+        print(f"Error retrieving professor {name}: {ex}", file=sys.stderr)
 
 
 def get_reviews(name: str):
-    with sqlalchemy.orm.Session(engine) as session:
-        query = session.query(Review).filter(name == name)
-        table = query.all()
-        return table
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            query = session.query(Review).filter(Review.name == name)
+            table = query.all()
+            return table
+    except sqlalchemy.exc.SQLAlchemyError as ex:
+        print(f"Error retrieving reviews for professor {name}: {ex}", file=sys.stderr)
 
 
-def _add_professor(name, dept, rating=0, numratings=0):
-    with sqlalchemy.orm.Session(engine) as session:
 
-        professor = Professor(
-            name=name, department=dept, rating=rating, numratings=numratings
-        )
-        session.add(professor)
-        session.commit()
-
+def _add_professor(name, dept):
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            professor = Professor(
+                name=name, department=dept, rating=0, content=0,
+                delivery=0, availability=0, organization=0, numratings=0
+            )
+            session.add(professor)
+            session.commit()
+    except sqlalchemy.exc.SQLAlchemyError as ex:
+        print(f"Error adding professor {name}: {ex}", file=sys.stderr)
 
 def prof_exists(name):
-    with sqlalchemy.orm.Session(engine) as session:
-        return bool(session.query(Professor).filter_by(name=name).first())
+    try:
+        with sqlalchemy.orm.Session(engine) as session:
+            return bool(session.query(Professor).filter_by(name=name).first())
+    except sqlalchemy.exc.SQLAlchemyError as ex:
+        print(f"Error checking if professor {name} exists: {ex}", file=sys.stderr)
     # if len(session.query(Professor).filter_by(name == name).all()) != 0:
     #     return True
     # else:
@@ -72,6 +88,7 @@ def add_review(
 
         review = Review(
             name=name,
+            rating = (content + delivery + availability + organization)/4,
             content=content,
             delivery=delivery,
             availability=availability,
@@ -92,10 +109,17 @@ def add_review(
         print(prof.name + ": ")
         print(prof.numratings)
         print(prof.rating)
+        prof.content = prof.content 
         prof.rating = (
             prof.rating * (prof.numratings - 1)
             + (content + delivery + availability + organization) / 4
         ) / prof.numratings
+
+        prof.content = ((prof.content*(prof.numratings-1))+content)/prof.numratings
+        prof.delivery = ((prof.delivery*(prof.numratings-1))+delivery)/prof.numratings
+        prof.availability = ((prof.availability*(prof.numratings-1))+availability)/prof.numratings
+        prof.organization = ((prof.organization*(prof.numratings-1))+organization)/prof.numratings
+
         print(prof.rating)
         session.commit()
         session.flush()
@@ -103,11 +127,18 @@ def add_review(
 
 # test functions
 def main():
+    try:
+        add_review("Kayla", "cos", 4, 4, 4, 4, "asdfa", "asdfad")
+        add_review("Kayla", "cos", 1, 1, 1, 1, "asdfa", "asdfad")
+        add_review("Kohei", "orf", 5, 5, 5, 5, "lalala", "lalalal")
+        print(get_all_professors())
+    except Exception as ex:
+        print(f"An error occurred in the main function: {ex}", file=sys.stderr)
+
     # add_professor("Robert", "COS", 1.3)
     # add_review("Robert", 1.3, 1.3, 1.3, 1.3,"Hello", "hello",)
     # print(get_all_professors())
     # print(get_reviews("Robert"))
-    add_review("Kayla", "cos", 4, 4, 4, 4, "asdfa", "asdfad")
     # print('')
     # add_review('Bob', 'cos', 3, 4, 5, 3, 3, 'asdfa', 'asdfad')
 
