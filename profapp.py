532e77a3c08b13bb1f4da510d9cf0cb0aa8f5b9e
Eden Bendory <eb1889@princeton.edu>
2023-11-17 12:09:47 -0500

    puts back auth

--- profapp.py
+++ profapp.py
@@ -29,11 +29,13 @@ def logoutcas():
 @app.route("/", methods=["GET"])
 @app.route("/index", methods=["GET"])
 def index():
-    # username = auth.authenticate()
+    username = auth.authenticate()
 
     professors = db.get_all_professors()
 
-    html_code = flask.render_template("index.html", professors=professors)
+    html_code = flask.render_template(
+        "index.html", professors=professors, username=username
+    )
     response = flask.make_response(html_code)
     return response
 
